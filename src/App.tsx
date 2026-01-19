7cf352bdb056816606dabe8be89e86bbc18b966b
angelc2@unlv.nevada.edu <148724296+charlilize@users.noreply.github.com>
2026-01-18 18:52:59 -0800

    results and map outline

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-export function App() {
-  return (
-    <div className="flex items-center justify-center min-h-screen">
-      <div className="font-medium">Hello World</div>
-    </div>
-  )
-}
 
-export default App
+export function App() {
+  return (  
+  <>
+  <div className='flex items-center justify-center h-screen p-7'>
+  <div className='w-3/5 h-full bg-gray-100'></div>
+  <div className='w-2/5 h-full bg-gray-200'><h1>Map</h1></div>
+  </div>
+</>) }
+
+export default App;

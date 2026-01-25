a1676749001bf4a1a808ad40d683401b9292db27
angelc2@unlv.nevada.edu <148724296+charlilize@users.noreply.github.com>
2026-01-25 14:00:44 -0800

    query ready for api

--- src/App.tsx
+++ src/App.tsx
@@ -4,84 +4,54 @@ import {
   CardDescription,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
-function App() {
-  return (
-    <div className="h-screen flex flex-col overflow-hidden">
-    <Header />
-      <div className='flex items-center justify-center flex-1 min-h-0 p-7'>
-      {/* Place Results */}
-      <div className='w-[51.5%] h-full p-2 pt-12 flex flex-wrap gap-4 content-start justify-center overflow-y-auto scrollbar-none'>
-        <Card className="relative w-full max-w-xs overflow-hidden pt-0">
-          <div className="bg-primary absolute inset-0 z-30 aspect-4/3 opacity-50 mix-blend-color" />
-          <img
-            src="https://images.unsplash.com/photo-1604076850742-4c7221f3101b?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            alt="Photo by mymind on Unsplash"
-            title="Photo by mymind on Unsplash"
-            className="relative z-20 aspect-4/3 w-full object-cover brightness-60 grayscale"
-          />
-          <CardHeader>
-            <CardTitle>Observability Plus is replacing Monitoring</CardTitle>
-            <CardDescription>
-              Switch to the improved way to explore your data, with natural
-              language.
-            </CardDescription>
-          </CardHeader>
-        </Card>
-        <Card className="relative w-full max-w-xs overflow-hidden pt-0">
-          <div className="bg-primary absolute inset-0 z-30 aspect-4/3 opacity-50 mix-blend-color" />
-          <img
-            src="https://images.unsplash.com/photo-1604076850742-4c7221f3101b?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            alt="Photo by mymind on Unsplash"
-            title="Photo by mymind on Unsplash"
-            className="relative z-20 aspect-4/3 w-full object-cover brightness-60 grayscale"
-          />
-          <CardHeader>
-            <CardTitle>Observability Plus is replacing Monitoring</CardTitle>
-            <CardDescription>
-              Switch to the improved way to explore your data, with natural
-              language. 
-            </CardDescription>
-          </CardHeader>
-        </Card>
-        <Card className="relative w-full max-w-xs overflow-hidden pt-0">
-          <div className="bg-primary absolute inset-0 z-30 aspect-4/3 opacity-50 mix-blend-color" />
-          <img
-            src="https://images.unsplash.com/photo-1604076850742-4c7221f3101b?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            alt="Photo by mymind on Unsplash"
-            title="Photo by mymind on Unsplash"
-            className="relative z-20 aspect-4/3 w-full object-cover brightness-60 grayscale"
-          />
-          <CardHeader>
-            <CardTitle>Observability Plus is replacing Monitoring</CardTitle>
-            <CardDescription>
-              Switch to the improved way to explore your data, with natural
-              language.
-            </CardDescription>
-          </CardHeader>
-        </Card>
-        <Card className="relative w-full max-w-xs overflow-hidden pt-0">
-          <div className="bg-primary absolute inset-0 z-30 aspect-4/3 opacity-50 mix-blend-color" />
-          <img
-            src="https://images.unsplash.com/photo-1604076850742-4c7221f3101b?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            alt="Photo by mymind on Unsplash"
-            title="Photo by mymind on Unsplash"
-            className="relative z-20 aspect-4/3 w-full object-cover brightness-60 grayscale"
-          />
-          <CardHeader>
-            <CardTitle>Observability Plus is replacing Monitoring</CardTitle>
-            <CardDescription>
-              Switch to the improved way to explore your data, with natural
-              language.
-            </CardDescription>
-          </CardHeader>
-        </Card>
-      </div>
-      {/* Map */}
-      <div className='w-[48.5%] h-[82vh] bg-gray-200 mr-5'><h1>Map</h1></div>
-      </div>
-    </div>
-  )
+} from "@/components/ui/card";
+
+import { useEffect, useState } from "react";
+
+interface q {
+  location: string;
+  lookingFor: string;
+  long: number;
+  lat: number;
 }
 
-export default App
+function App() {
+  const [query, setQuery] = useState<q | null>(null);
+
+  useEffect(() => {
+    console.log("in app:", query);
+  }, [query]);
+
+  return (
+    <div className="h-screen flex flex-col overflow-hidden">
+      <Header getQuery={setQuery} />
+      <div className="flex items-center justify-center flex-1 min-h-0 p-7">
+        {/* Place Results */}
+        <div className="w-[51.5%] h-full p-2 pt-12 flex flex-wrap gap-4 content-start justify-center overflow-y-auto scrollbar-none">
+          <Card className="relative w-full max-w-xs overflow-hidden pt-0">
+            <div className="bg-primary absolute inset-0 z-30 aspect-4/3 opacity-50 mix-blend-color" />
+            <img
+              src="https://images.unsplash.com/photo-1604076850742-4c7221f3101b?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt="Photo by mymind on Unsplash"
+              title="Photo by mymind on Unsplash"
+              className="relative z-20 aspect-4/3 w-full object-cover brightness-60 grayscale"
+            />
+            <CardHeader>
+              <CardTitle>Observability Plus is replacing Monitoring</CardTitle>
+              <CardDescription>
+                Switch to the improved way to explore your data, with natural
+                language.
+              </CardDescription>
+            </CardHeader>
+          </Card>
+        </div>
+        {/* Map */}
+        <div className="w-[48.5%] h-[82vh] bg-gray-200 mr-5">
+          <h1>Map</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,11 +1,7 @@
-import { useState, useRef } from "react"
-import { Button } from "@/components/ui/button"
-import {
-  Field,
-  FieldGroup,
-  FieldLabel,
-} from "@/components/ui/field"
-import { Input } from "@/components/ui/input"
+import { useState, useRef } from "react";
+import { Button } from "@/components/ui/button";
+import { Field, FieldGroup, FieldLabel } from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
 
 import {
   Combobox,
@@ -14,160 +10,196 @@ import {
   ComboboxInput,
   ComboboxItem,
   ComboboxList,
-} from "@/components/ui/combobox"
+} from "@/components/ui/combobox";
 
-const MIN_ADDRESS_LENGTH = 3
-const DELAY = 300
+const MIN_ADDRESS_LENGTH = 3;
+const DELAY = 300;
 
-type cityInfo = {
+interface cityInfo {
   formatted: string;
   state: string;
   city: string;
-  longitude: number,
-  latitude: number,
+  country: string;
+  longitude: number;
+  latitude: number;
 }
 
-function Header() {
-  const [cities, setCities] = useState<cityInfo[]>([])                    // Cities to show in dropdown
-  const [selectedCity, setSelectedCity] = useState<cityInfo | null>(null)  // City that is selected
-  const [cityQuery, setCityQuery] = useState("")                          // Query to search for cities
-  const [lookingForQuery, setLookingForQuery] = useState("")              // Query to search for places
-  
-  const debounceTimeoutRef = useRef<ReturnType<typeof setTimeout> | null>(null)  // set timer for 300ms
-  const abortControllerRef = useRef<AbortController | null>(null)                // like the cancel button for api requests
+function Header({
+  getQuery,
+}: {
+  getQuery: (query: {
+    location: string;
+    lookingFor: string;
+    long: number;
+    lat: number;
+  }) => void;
+}) {
+  const [cities, setCities] = useState<cityInfo[]>([]); // Cities to show in dropdown
+  const [selectedCity, setSelectedCity] = useState<cityInfo | null>(null); // City that is selected
+  const [cityQuery, setCityQuery] = useState(""); // Query to search for cities
+  const [lookingForQuery, setLookingForQuery] = useState(""); // Query to search for places
+
+  const debounceTimeoutRef = useRef<ReturnType<typeof setTimeout> | null>(null); // set timer for 300ms
+  const abortControllerRef = useRef<AbortController | null>(null); // like the cancel button for api requests
 
   const handleCityInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const currentValue = e.target.value
-    setCityQuery(currentValue)
+    const currentValue = e.target.value;
+    setCityQuery(currentValue);
 
     // remove timer if user typed again before 300ms passed
     if (debounceTimeoutRef.current) {
-      clearTimeout(debounceTimeoutRef.current)
+      clearTimeout(debounceTimeoutRef.current);
     }
 
     // remove previous api request if user typed again before 300ms passed
     if (abortControllerRef.current) {
-      abortControllerRef.current.abort()
+      abortControllerRef.current.abort();
     }
 
     // Clear results if input is empty or too short
     if (!currentValue || currentValue.length < MIN_ADDRESS_LENGTH) {
-      setCities([]) // clear dropdown
-      return
+      setCities([]); // clear dropdown
+      return;
     }
 
     // store id of timer in ref
-    // start a timer for 300ms in background before making api request 
+    // start a timer for 300ms in background before making api request
     // (if user typed again before 300ms passed, timer is cleared above via new function call and new timer is created)
     debounceTimeoutRef.current = setTimeout(async () => {
       // create a way to cancel this specific request later
-      abortControllerRef.current = new AbortController()
+      abortControllerRef.current = new AbortController();
 
       try {
-        const apiKey = import.meta.env.VITE_GEOAPIFY_API_KEY
-        const url = `https://api.geoapify.com/v1/geocode/autocomplete?text=${encodeURIComponent(currentValue)}&type=city&limit=5&format=json&apiKey=${apiKey}`
+        const apiKey = import.meta.env.VITE_GEOAPIFY_API_KEY;
+        const url = `https://api.geoapify.com/v1/geocode/autocomplete?text=${encodeURIComponent(currentValue)}&type=city&limit=5&format=json&apiKey=${apiKey}`;
 
         // make api request and link to stop button
-        const response = await fetch(url, { 
-          signal: abortControllerRef.current.signal 
-        })
+        const response = await fetch(url, {
+          signal: abortControllerRef.current.signal,
+        });
 
         if (response.ok) {
           // if request is successful, get data
-          const data = await response.json()
+          const data = await response.json();
           // get just city names from data
           const cities = (data.results || []).map(
-            (result: { formatted: string,  city: string, state: string, lon: number, lat: number }) => ({
-              formatted: result.formatted, 
+            (result: {
+              formatted: string;
+              city: string;
+              state: string;
+              country: string;
+              lon: number;
+              lat: number;
+            }) => ({
+              formatted: result.formatted,
               city: result.city,
               state: result.state,
+              country: result.country,
               longitude: result.lon,
-              latitude: result.lat
-            })
-          )
+              latitude: result.lat,
+            }),
+          );
 
           // set city names in dropdown
-          setCities(cities)
+          setCities(cities);
         }
       } catch (error) {
         // Ignore abort errors (user typed again before request finished)
         if (error instanceof Error && error.name !== "AbortError") {
-          console.error("Geocoding error:", error)
+          console.error("Geocoding error:", error);
         }
       }
-    }, DELAY)
-  }
-  
+    }, DELAY);
+  };
+
   const handleCitySelect = (item: cityInfo) => {
-    setCityQuery(item.formatted)
-    setSelectedCity(item)
-  }
+    setCityQuery(item.formatted);
+    setSelectedCity(item);
+  };
 
-  const handleLookingForInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const currentValue = e.target.value
-    setLookingForQuery(currentValue)
-  }
+  const handleLookingForInputChange = (
+    e: React.ChangeEvent<HTMLInputElement>,
+  ) => {
+    const currentValue = e.target.value;
+    setLookingForQuery(currentValue);
+  };
 
-  
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    console.log("Form submitted")
+    e.preventDefault();
+    console.log("Form submitted");
 
     if (selectedCity) {
-      console.log(selectedCity.city, selectedCity.state, lookingForQuery)
-      console.log("longitude:", selectedCity.longitude)
-      console.log("latitude:", selectedCity.latitude)
-    }   
-  }
-  
-  return (
-    <div className='flex items-center justify-between bg-gray-200 p-5'>
-     <div className='flex items-center gap-2'>
-      <h1 className='text-2xl font-bold'>Place Finder</h1>
-     </div>
-     <form className='w-full max-w-3xl' onSubmit={handleSubmit}>
-      <FieldGroup>
-        <div className='grid grid-cols-[1fr_1fr_auto] gap-4 items-end'>
-        <Field>
-          <FieldLabel>Location</FieldLabel>
-          <Combobox items={cities}>
-              <ComboboxInput 
-                value={cityQuery}
-                type="text" 
-                placeholder="Search city..." 
-                showTrigger={false} 
-                className="bg-white"
-                onChange={handleCityInputChange}  
-                required
-              />
-              <ComboboxContent>
-                <ComboboxEmpty>
-                  {cityQuery.length < MIN_ADDRESS_LENGTH 
-                    ? "Type at least 3 characters..." 
-                    : "No cities found."}
-                </ComboboxEmpty>
-                <ComboboxList>
-                  {(item: cityInfo) => (
-                    <ComboboxItem key={item.formatted} value={item.formatted} onClick={() => handleCitySelect(item)}>{item.formatted}</ComboboxItem>
-                  )}
-                </ComboboxList>
-              </ComboboxContent>
-            </Combobox>
-        </Field>
-        <Field>
-          <FieldLabel>Looking for</FieldLabel>
-          <Input type="text" placeholder="Parks, coffee, museums..." className='bg-white' required onChange={handleLookingForInputChange}/>
-        </Field>
-        <Button type="submit" size="lg">Search</Button>
-        </div>
-      </FieldGroup>
-     </form>
+      getQuery({
+        location: `${selectedCity.city}, ${selectedCity.state}, ${selectedCity.country}`,
+        lookingFor: lookingForQuery,
+        long: selectedCity.longitude,
+        lat: selectedCity.latitude,
+      });
+    }
+  };
 
-     <div className='flex items-center gap-2'>
-      <h1>About</h1>
-     </div>
+  return (
+    <div className="flex items-center justify-between bg-gray-200 p-5">
+      <div className="flex items-center gap-2">
+        <h1 className="text-2xl font-bold">Place Finder</h1>
+      </div>
+      <form className="w-full max-w-3xl" onSubmit={handleSubmit}>
+        <FieldGroup>
+          <div className="grid grid-cols-[1fr_1fr_auto] gap-4 items-end">
+            <Field>
+              <FieldLabel>Location</FieldLabel>
+              <Combobox items={cities}>
+                <ComboboxInput
+                  value={cityQuery}
+                  type="text"
+                  placeholder="Search city..."
+                  showTrigger={false}
+                  className="bg-white"
+                  onChange={handleCityInputChange}
+                  required
+                />
+                <ComboboxContent>
+                  <ComboboxEmpty>
+                    {cityQuery.length < MIN_ADDRESS_LENGTH
+                      ? "Type at least 3 characters..."
+                      : "No cities found."}
+                  </ComboboxEmpty>
+                  <ComboboxList>
+                    {(item: cityInfo) => (
+                      <ComboboxItem
+                        key={item.formatted}
+                        value={item.formatted}
+                        onClick={() => handleCitySelect(item)}
+                      >
+                        {item.formatted}
+                      </ComboboxItem>
+                    )}
+                  </ComboboxList>
+                </ComboboxContent>
+              </Combobox>
+            </Field>
+            <Field>
+              <FieldLabel>Looking for</FieldLabel>
+              <Input
+                type="text"
+                placeholder="Parks, coffee, museums..."
+                className="bg-white"
+                required
+                onChange={handleLookingForInputChange}
+              />
+            </Field>
+            <Button type="submit" size="lg">
+              Search
+            </Button>
+          </div>
+        </FieldGroup>
+      </form>
+
+      <div className="flex items-center gap-2">
+        <h1>About</h1>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
